f0d27713dd8767c5772438bc51002cb51015f6f9
oleg <perchykoleg@gmail.com>
2019-11-20 16:36:48 -0600

    update data model

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -3,36 +3,17 @@
     <h1 class="title">oleg.cloud</h1>
 
     <div class="social-icons">
-      <a href="https://www.linkedin.com/in/olegperchyk/" target="_blank">
+      <a
+        v-for="button in socialButtons"
+        :key="button.id"
+        :href="button.link"
+        target="_blank"
+      >
         <img
           class="icon-fade"
-          src="../assets/icons/linkedin.svg"
-          alt="linkedin"
-          title="LinkedIn"
-        />
-      </a>
-      <a href="https://github.com/himynameisoleg" target="_blank">
-        <img
-          class="icon-fade"
-          src="../assets/icons/github.svg"
-          alt="github"
-          title="GitHub"
-        />
-      </a>
-      <a href="https://twitter.com/himynameisoleg" target="_blank">
-        <img
-          class="icon-fade"
-          src="../assets/icons/twitter.svg"
-          alt="twitter"
-          title="Twitter"
-        />
-      </a>
-      <a href="https://himynameisoleg.com" target="_blank">
-        <img
-          class="icon-fade"
-          src="../assets/icons/write.svg"
-          alt="blog"
-          title="himynameisoleg.com"
+          :src="getImageURL(button.icon)"
+          :alt="button.alt"
+          :title="button.title"
         />
       </a>
     </div>
@@ -50,8 +31,39 @@ export default {
   name: "Logo",
   data() {
     return {
-      traits: ["learner", "intellection", "input", "analytical", "focus"]
+      traits: ["learner", "intellection", "input", "analytical", "focus"],
+      socialButtons: [
+        {
+          link: "https://www.linkedin.com/in/olegperchyk/",
+          icon: "linkedin.svg",
+          alt: "linkedin",
+          title: "LinkedIn"
+        },
+        {
+          link: "https://github.com/himynameisoleg",
+          icon: "github.svg",
+          alt: "github",
+          title: "GitHub"
+        },
+        {
+          link: "https://twitter.com/himynameisoleg",
+          icon: "twitter.svg",
+          alt: "twitter",
+          title: "Twitter"
+        },
+        {
+          link: "https://himynameisoleg.com",
+          icon: "write.svg",
+          alt: "blog",
+          title: "himynameisoleg.com"
+        }
+      ]
     };
+  },
+  methods: {
+    getImageURL(name) {
+      return require(`../assets/icons/${name}`);
+    }
   }
 };
 </script>
