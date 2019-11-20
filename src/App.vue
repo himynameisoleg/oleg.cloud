ed9eb2b8fb2fde77357acbe496d622f3371d5562
oleg <perchykoleg@gmail.com>
2019-11-19 23:33:37 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <Logo />
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Logo from "./components/Logo.vue";
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
+    name: "app",
+    components: {
+        Logo
+    }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background: url("./assets/desk-bg.jpg") no-repeat center center;
+    background-size: cover;
 }
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="Logo">
+        <h1>oleg perchyk</h1>
+        <a href="https://www.linkedin.com/in/olegperchyk/" target="_blank">
+            <img src="../assets/icons/linkedin.svg" />
+        </a>
+        <a href="https://github.com/himynameisoleg" target="_blank">
+            <img src="../assets/icons/github.svg" />
+        </a>
+        <a href="https://twitter.com/himynameisoleg" target="_blank">
+            <img src="../assets/icons/twitter.svg" />
+        </a>
+
+        <p>
+            <span class="trait" v-for="trait in traits" :key="trait.id">{{ `${trait}.` }}</span>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Logo",
+    data() {
+        return {
+            traits: ["learner", "intellection", "input", "analytical", "focus"]
+        };
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.Logo {
+    margin-top: 15vh;
+}
+.trait {
+    margin: 0 0.5em 0 0.5em;
+}
+img {
+    margin: 0 0.5em 0 0.5em;
+    &:hover {
+        // cursor: pointer;
+        opacity: 0.2;
+        transition: opacity 0.1s ease;
+    }
+}
+</style>
