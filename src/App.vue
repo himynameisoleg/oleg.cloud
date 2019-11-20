ca21f394ae695d9391218c5dbb3c1c3ba0373321
oleg <perchykoleg@gmail.com>
2019-11-19 23:46:32 -0600

    prettier

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-    <div id="app">
-        <Logo />
-    </div>
+  <div id="app">
+    <Logo />
+  </div>
 </template>
 
 <script>
 import Logo from "./components/Logo.vue";
 
 export default {
-    name: "app",
-    components: {
-        Logo
-    }
+  name: "app",
+  components: {
+    Logo
+  }
 };
 </script>
 
 <style lang="scss">
 #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
-    text-align: center;
-    color: #2c3e50;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    background: url("./assets/desk-bg.jpg") no-repeat center center;
-    background-size: cover;
+  background: url("./assets/desk-bg.jpg") no-repeat center center;
+  background-size: cover;
 }
 </style>
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,47 +1,49 @@
 <template>
-    <div class="Logo">
-        <h1>oleg.cloud</h1>
-        <a href="https://www.linkedin.com/in/olegperchyk/" target="_blank">
-            <img src="../assets/icons/linkedin.svg" />
-        </a>
-        <a href="https://github.com/himynameisoleg" target="_blank">
-            <img src="../assets/icons/github.svg" />
-        </a>
-        <a href="https://twitter.com/himynameisoleg" target="_blank">
-            <img src="../assets/icons/twitter.svg" />
-        </a>
+  <div class="Logo">
+    <h1>oleg.cloud</h1>
+    <a href="https://www.linkedin.com/in/olegperchyk/" target="_blank">
+      <img src="../assets/icons/linkedin.svg" />
+    </a>
+    <a href="https://github.com/himynameisoleg" target="_blank">
+      <img src="../assets/icons/github.svg" />
+    </a>
+    <a href="https://twitter.com/himynameisoleg" target="_blank">
+      <img src="../assets/icons/twitter.svg" />
+    </a>
 
-        <p>
-            <span class="trait" v-for="trait in traits" :key="trait.id">{{ `${trait}.` }}</span>
-        </p>
-    </div>
+    <p>
+      <span class="trait" v-for="trait in traits" :key="trait.id">{{
+        `${trait}.`
+      }}</span>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Logo",
-    data() {
-        return {
-            traits: ["learner", "intellection", "input", "analytical", "focus"]
-        };
-    }
+  name: "Logo",
+  data() {
+    return {
+      traits: ["learner", "intellection", "input", "analytical", "focus"]
+    };
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .Logo {
-    margin-top: 15vh;
+  margin-top: 15vh;
 }
 .trait {
-    margin: 0 0.5em 0 0.5em;
+  margin: 0 0.5em 0 0.5em;
 }
 img {
-    margin: 0 0.5em 0 0.5em;
-    &:hover {
-        // cursor: pointer;
-        opacity: 0.2;
-        transition: opacity 0.1s ease;
-    }
+  margin: 0 0.5em 0 0.5em;
+  &:hover {
+    // cursor: pointer;
+    opacity: 0.2;
+    transition: opacity 0.1s ease;
+  }
 }
 </style>
